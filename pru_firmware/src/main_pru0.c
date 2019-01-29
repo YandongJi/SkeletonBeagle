b6e2084831592d1d9ab3e22f7890087db09fd13e
Ron Fearing <ronf@eecs.berkeley.edu>
2019-01-29 10:54:59 -0800

    starting to add a to d to main_pru0.c

--- pru_firmware/src/main_pru0.c
+++ pru_firmware/src/main_pru0.c
@@ -39,20 +39,37 @@
 #include <pru_ctrl.h>
 #include "resource_table_pru1.h"
 
+#define ENCODER_MEM_OFFSET	16
+#define PRU_SHAREDMEM 0x10000 // shared memory with Cortex A8?
+// pru shared memory pointer
+static volatile unsigned int* shared_mem_32bit_ptr = NULL;
+
+/* prototypes */
+extern void init_adc(void);
+extern uint16_t read_adc(uint16_t);
+
 // The function is defined in pru1_asm_blinky.asm in same dir
 // We just need to add a declaration here, the defination can be
 // seperately linked
 extern void start(void);
 
 void main(void)
-{
+{	int atod_value =0;
 	/* Clear SYSCFG[STANDBY_INIT] to enable OCP master port */
 	CT_CFG.SYSCFG_bit.STANDBY_INIT = 0;
 	
 	// Access PRU Shared RAM using Constant Table                    */
 	// C28 defaults to 0x00000000, we need to set bits 23:8 to 0x0100 in order to have it point to 0x00010000	 */
 	PRU0_CTRL.CTPPR0_bit.C28_BLK_POINTER = 0x0100;
-
+	
+	// try to access shared memory from C 
+	shared_mem_32bit_ptr = (volatile unsigned int *) PRU_SHAREDMEM;
+	shared_mem_32bit_ptr[ENCODER_MEM_OFFSET+2] = 0x5a5a;
+	
+	init_adc();
+//	atod_value = read_adc(0);
+	shared_mem_32bit_ptr[ENCODER_MEM_OFFSET+2] = atod_value;
+	
 	start();
 }
 
