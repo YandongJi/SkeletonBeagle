e865c5c24347ee98cea54f17f4d995a91c61010e
Ron Fearing <ronf@berkeley.edu>
2019-02-27 16:18:02 -0800

    cleaned out some old encoder reading code

--- LineCamera/LineCamera.c
+++ LineCamera/LineCamera.c
@@ -39,15 +39,14 @@ static void __signal_handler(__attribute__ ((unused)) int dummy)
 
 
 int main()
-{	long value = 0;
-	long i, j;
+{	long i, j;
 	int pin1 = UART1_HEADER_PIN_3;	///< P9_26, normally UART1 RX
 	int pin2 = UART1_HEADER_PIN_4;	///< P9_24, normally UART1 TX
 	
 	int linescan[128]; // array to hold line scan data
 	
 	printf("LineScan camera test. Using Debian and PRU, not working with Ubuntu. \n");
-	printf("Make sure pru_firmware is installed.v1.00 2/27/2019\n");
+	printf("Make sure pru_firmware is installed. v1.00 2/27/2019\n");
 	
 	
 	// initialize PRU hardware first
@@ -74,36 +73,14 @@ int main()
 	signal(SIGINT, __signal_handler);
 	running=1;
 
-	
-	printf("\nRaw encoder position\n");
-	printf("      E4   |");
-	printf(" \n");
-	rc_encoder_pru_write(1); //set to non-zero value so can verify read cycles
-	start_time = rc_nanos_thread_time();
-	for(i = 0; i < 1000000; i++)
-	{  value = value + rc_encoder_pru_read();
-	}
-	end_time = rc_nanos_since_boot();
-	run_time = end_time - start_time;
-	printf("Encoder 1e6 reads. Total = %ld. Takes: %" PRIu64 "us\n",value, run_time/1000);
-
+	//fetches a pointer to the beginning of the PRU shared memory where line scan data is stored
 	shared_mem_32bit_ptr = rc_pru_shared_mem_ptr();
 	if(shared_mem_32bit_ptr==NULL){
 		fprintf(stderr, "ERROR in rc_encoder_pru_init, failed to map shared memory pointer\n");
 		return -1;
 	} 
+	printf("Initialization Complete\n");
 	
-// //	while(running){
-// 		printf("\r%10d |", rc_encoder_pru_read());
-// 		printf("  r3=%x", 
-// 		(int) shared_mem_32bit_ptr[ENCODER_MEM_OFFSET+1]);  // supposed to be written from PRU, next word above
-// 		printf("  shared_mem_32bit_ptr[ENCODER_MEM_OFFSET+2]=%x", 
-// 		(int) shared_mem_32bit_ptr[ENCODER_MEM_OFFSET+2]);  // supposed to be written from PRU, next word above
-// 		fflush(stdout);
-// 		rc_usleep(50000);
-//	}
-
-	printf("\n");
 	start_time = rc_nanos_since_boot();
 	for(j = 0; j< 1000; j++)
 	{	shared_mem_32bit_ptr[ENCODER_MEM_OFFSET+1] = 1; // set flag to start conversion by PRU
